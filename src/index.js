fccc34f241a94aacf3d77aa4a7ec11e38bbe386d
Gaurav-461 <mauryagaurav1612@gmail.com>
2026-04-01 02:30:59 +0530

    implement the websocket server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import http from "http"
 import app from "./app.js";
 import matchRouter from "./routes/matches.js";
+import { attachWebSocketServer } from "./ws/server.js";
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || '0.0.0.0'
+
+const server = http.createServer(app);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Sportz API" });
@@ -9,9 +15,15 @@ app.get("/", (req, res) => {
 
 app.use("/matches", matchRouter);
 
-app
-  .listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server
+  .listen(PORT,HOST, () => {
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
   })
   .on("error", (err) => {
     console.error("Server failed to start:", err.message);
---
+++ src/ws/server.js
@@ -0,0 +1,39 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+function sendJson(socket, payload) {
+  if (socket.readyState !== WebSocket.OPEN) return;
+
+  socket.send(JSON.stringify(payload));
+}
+
+function broadcast(wss, payload) {
+  
+  for (const client of wss.clients) {
+    if (client.readyState !== WebSocket.OPEN) continue;
+
+    client.send(JSON.stringify(payload))
+  };
+}
+
+export function attachWebSocketServer(server) {
+  const wss = new WebSocketServer({ server: server, path: "/ws", maxPayload: 1024 * 1024 });
+
+  wss.on("connection", (socket) => {
+    sendJson(socket, {
+      type: "Welcome to Sportz wss",
+    })
+  });
+
+  wss.on("error", (error) => {
+    console.error("WebSocket server error:", error);
+  });
+
+  function broadcastMatchCreated(match) {
+    broadcast(wss, {
+      type: "match_created",
+      data: match,
+    });
+  }
+
+  return { broadcastMatchCreated };
+}
